ca2e7807ad84b2f7de6e97913ec62c1e5af19c19
rnjs010 <kyuri4809@naver.com>
2023-05-06 13:20:55 +0900

    mainpage추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import StartPage from './pages/StartPage/StartPage';
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <StartPage />
+      {/* <StartPage /> */}
+      <MainPage />
     </div>
   );
 }
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Box from './Box/Box';
+import './MainPage.scss';
+
+function MainPage() {
+    return (
+        <div>
+          <div className='container'>
+            <Box name="아이유"/>
+            <Box name="손흥민"/>
+            <Box name="이순신"/>
+            <Box name="잡스"/>
+            <Box name="박지성"/>
+            <Box name="몰라"/>
+          </div>
+        </div>
+    );
+}
+
+export default MainPage;
