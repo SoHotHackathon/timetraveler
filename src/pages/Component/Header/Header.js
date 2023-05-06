172812b2a4cd7332497651905eec3d6fd4583e1b
헥스/박해승 <2001phs92@gmail.com>
2023-05-06 14:14:33 +0900

    Create StartPage

--- src/pages/Component/Header/Header.js
+++ src/pages/Component/Header/Header.js
@@ -8,7 +8,7 @@ function Header(){
     return <header>
       <h1>시간여행</h1>
       <div className='buttonGroup'>
-        <button className='start'>시작하기</button>
+        <button onlcick='' className='start'>시작하기</button>
         <button className='iconBtn'><FontAwesomeIcon icon={faHouse}/></button>
         <button className='iconBtn'><FontAwesomeIcon icon={faGear}/></button>
       </div>
