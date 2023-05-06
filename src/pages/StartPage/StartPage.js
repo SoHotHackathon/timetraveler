f063f8f647e24891fc2802dd83d924408407d880
rnjs010 <kyuri4809@naver.com>
2023-05-07 01:57:12 +0900

    .

--- src/pages/StartPage/StartPage.js
+++ src/pages/StartPage/StartPage.js
@@ -20,20 +20,28 @@ function StartPage() {
                 <div><FontAwesomeIcon icon={faAnglesDown} /></div>
             </div>
             <div className='introTxt'>어서와요, 시간여행을 떠나볼까요?<br /><br />이순신, 아이유, 스티브잡스와 함께한 대화를 시작해봅시다!</div>
+            
             <section className='scene two'></section>
             <div className='scrollMark'>
                 <div>Scroll</div>
                 <div><FontAwesomeIcon icon={faAnglesDown} /></div>
             </div>
+            <div className='introTxt'>과거와 미래를 넘나들며 새로운 이야기를 만나보세요.</div>
+            
+            <section className='scene three'></section>
             <div className='scrollMark'>
                 <div>Scroll</div>
                 <div><FontAwesomeIcon icon={faAnglesDown} /></div>
             </div>
-            <div className='introTxt'>과거와 미래를 넘나들며 새로운 이야기를 만나보세요.</div>
-            <section className='scene three'></section>
             <div className='introTxt'>세계적인 인물들과 대화하는 시간여행, 지금부터 시작합니다.</div>
+            
             <section className='scene four'></section>
+            <div className='scrollMark'>
+                <div>Scroll</div>
+                <div><FontAwesomeIcon icon={faAnglesDown} /></div>
+            </div>
             <div className='introTxt'>과거와 미래를 넘나들며 새로운 시각을 발견해보세요.</div>
+            
             <section className='scene five'></section>
         </div>
     );
