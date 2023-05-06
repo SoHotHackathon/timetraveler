62542f0fd712290e4ec2a0270d7688e3c22378ee
rnjs010 <kyuri4809@naver.com>
2023-05-07 07:18:07 +0900

    디테일 수정

--- src/pages/ChatPage/ChatPage.js
+++ src/pages/ChatPage/ChatPage.js
@@ -28,6 +28,12 @@ function ChatPage() {
           <li className='sent message'>
             <span>공부가 하기 싫어요</span>
           </li>
+          <li className='sent message'>
+            <span>공부가 하기 싫어요</span>
+          </li>
+          <li className='received message'>
+            <span>열심히 하세요.</span>
+          </li>
         </ul>
       </div>
       <div className='input-container'>
