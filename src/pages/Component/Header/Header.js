31d3e537e85aaa5614c21c7ca39c35b5cc31782c
헥스/박해승 <2001phs92@gmail.com>
2023-05-06 12:52:50 +0900

    Create Header

---
+++ src/pages/Component/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faGear } from '@fortawesome/free-solid-svg-icons';
+
+
+function Header(){
+    return <header>
+      <h1>시간여행</h1>
+      <div className='buttonGroup'>
+        <button className='start'>시작하기</button>
+        <button className='iconBtn'><FontAwesomeIcon icon={faHouse}/></button>
+        <button className='iconBtn'><FontAwesomeIcon icon={faGear}/></button>
+      </div>
+    </header>
+}
+
+export default Header;
--- src/pages/StartPage/StartPage.js
+++ src/pages/StartPage/StartPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './StartPage.scss';
+import Header from '../Component/Header/Header';
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+function Article(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
 
 function StartPage() {
     return (
         <div>
-            <p>alksdfasdkjf</p>
+            <Header></Header>
+            <Nav></Nav>
+            <Article></Article>
         </div>
     );
 }
