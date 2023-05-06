a6e247f34117a1c9b693b490c137b17482f50313
rnjs010 <kyuri4809@naver.com>
2023-05-06 15:13:13 +0900

    image 수정 중

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,21 +2,34 @@ import React from 'react';
 import Box from './Box/Box';
 import './MainPage.scss';
 import Header from '../Component/Header/Header';
+import CategoryBox from './CategoryBox/CategoryBox';
 
 function MainPage() {
-    return (
-        <div>
-          {/* <Header /> */}
-          <div className='container'>
-            <Box name="아이유"/>
-            <Box name="손흥민"/>
-            <Box name="이순신"/>
-            <Box name="잡스"/>
-            <Box name="박지성"/>
-            <Box name="몰라"/>
-          </div>
+  return (
+    <div>
+      {/* <Header /> */}
+      <div className='MainPage'>
+        <div className='container'>
+          <Box name="아이유" />
+          <Box name="손흥민" />
+          <Box name="이순신" />
+          <Box name="잡스" />
+          <Box name="박지성" />
+          <Box name="몰라" />
         </div>
-    );
+        <div className='category-list'>
+          <CategoryBox categoryname="철학자" />
+          <CategoryBox categoryname="공학자" />
+          <CategoryBox categoryname="종교인" />
+          <CategoryBox categoryname="예술가" />
+          <CategoryBox categoryname="과학자" />
+          <CategoryBox categoryname="운동선수" />
+          <CategoryBox categoryname="연예인" />
+        </div>
+      </div>
+    </div>
+
+  );
 }
 
 export default MainPage;
