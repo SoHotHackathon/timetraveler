73c194d3c745580abd413a175cecf0a8bb06ec4f
rnjs010 <kyuri4809@naver.com>
2023-05-07 04:28:15 +0900

    더미추가

--- src/pages/RecordPage/RecordPage.js
+++ src/pages/RecordPage/RecordPage.js
@@ -12,21 +12,21 @@ function RecordPage({ onClose }) {
           <div className='RecordPage-title'>시간여행의 기록</div>
           <div className='record-list'>
             <div className='record-box'>
-              <button className='record-pic'>인물사진</button>
-              <p className='record-name'>인물 이름</p>
-              <p className='record-category'>카테고리</p>
+              <button className='record-pic'></button>
+              <p className='record-name'>폰노이만</p>
+              <p className='record-category'>과학자</p>
               <p className='record-date'>2023년 05월 07일</p>
             </div>
             <div className='record-box'>
-              <button className='record-pic'>인물사진</button>
-              <p className='record-name'>인물 이름</p>
-              <p className='record-category'>카테고리</p>
+              <button className='record-pic'></button>
+              <p className='record-name'>빌게이츠</p>
+              <p className='record-category'>공학자</p>
               <p className='record-date'>2023년 05월 07일</p>
             </div>
             <div className='record-box'>
-              <button className='record-pic'>인물사진</button>
-              <p className='record-name'>인물 이름</p>
-              <p className='record-category'>카테고리</p>
+              <button className='record-pic'></button>
+              <p className='record-name'>세르게이 브린</p>
+              <p className='record-category'>공학자</p>
               <p className='record-date'>2023년 05월 07일</p>
             </div>
           </div>
